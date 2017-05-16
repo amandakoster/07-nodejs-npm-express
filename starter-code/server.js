4d56901e3dd1c486f30cea5db5e109c6bcec8fe6
Amanda Koster <amandakoster@gmail.com>
2017-05-16 13:45:18 -0700

    added a few possible answers

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,18 +1,22 @@
 'use strict';
 
 // TODO: Initialize your project using NPM to create and populate a package.json file
-
+npm init //https://docs.npmjs.com/getting-started/using-a-package.json
+npm install --save express
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
+// const express = require('express');
+
+
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
-
+//app.use((express.static('./public'));
 // TODO: Write a new route that will handle a request and send the new.html file back to the user
-
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
